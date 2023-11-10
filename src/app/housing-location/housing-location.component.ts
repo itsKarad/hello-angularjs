a147c58d49f2378289b13232597ad7a7b2c1a5ca
Aditya Karad <akarad@ee.iitr.ac.in>
2023-11-10 09:09:12 -0800

    Routing

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,17 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
-
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   template: `
     <section class="listing">
       <img class="listing-photo" [src]="housingLocationInput.photo" alt="Exterior photo of {{housingLocationInput.name}}">
       <h2 class="listing-heading">{{ housingLocationInput.name }}</h2>
       <p class="listing-location">{{ housingLocationInput.city}}, {{housingLocationInput.state }}</p>
+      <a [routerLink]="['/details', housingLocationInput.id]">Learn More</a>
     </section>
   `,
   styleUrl: './housing-location.component.css'
