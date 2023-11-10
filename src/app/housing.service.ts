4c7bb0ac0594a9b4fcd3e78ceb13933ab89b9346
Aditya Karad <akarad@ee.iitr.ac.in>
2023-11-10 10:00:13 -0800

    HTTP and async calls

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -109,17 +109,23 @@ export class HousingService {
     }
   ];
 
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
-  }
   
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
-  }
 
   submitApplication(firstName: string, lastName: string, email: string) {
     console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
   }
-  
+
+  url = 'http://localhost:3000/locations';
+
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
+  }
+
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(this.url + '/' + id);
+    return await data.json() ?? undefined;
+  }
+
   constructor() { }
 }
